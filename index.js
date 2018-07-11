f9aac81d19a74d3d849e44e52d35a3ea11d0bb5f
Levi Roberts <bugs181@gmail.com>
2018-07-11 00:13:13 -0500

    Added support for auth routes
    Examples:
    
    Simple:
      auth: function(callback) {
        console.log('auth called for /route')
        callback()
      }
    
    More complex:
      auth: {
        filter: ['readUser', updateUser],
        fn: function(callback) {
          console.log('auth called for /route')
          callback()
        }
      }

--- index.js
+++ index.js
@@ -3,14 +3,32 @@
 const parserName = 'esprima'
 const parser = require(parserName)
 
+let authRoutes = {}
+
 module.exports = function parseRoutes(routerObj, cfg, routePath) {
   if (!cfg || !cfg.verbs)
-    cfg = { verbs: { create: 'create', read: 'read', update: 'update', delete: 'delete', list: 'list' } }
+    cfg = { verbs: { create: 'create', read: 'read', update: 'update', delete: 'delete', list: 'list', auth: 'auth' } }
+
+  // FIXME: cfg = configFromObj() // Currently it's an all or nothing.
+  cfg.verbs.auth = 'auth'
 
   let parsedRoutes = []
 
   for (let routerProp of Object.keys(routerObj)) {
     const routeObj = routerObj[routerProp]
+
+    // Parse auth object
+    if (routerProp && routerProp == cfg.verbs.auth) {
+      const route = parseAuthRoute(routePath, routeObj, cfg)
+      if (!route)
+        continue
+
+      authRoutes[route.path] = route
+
+      // NOTE: Protocol might want to do some special processing, feedback requested.
+      parsedRoutes.push(route)
+      continue
+    }
 
     // Recurse into route object if needed (for nested routes)
     if (typeof routeObj == 'object' && !Array.isArray(routeObj)) {
@@ -25,7 +43,8 @@ module.exports = function parseRoutes(routerObj, cfg, routePath) {
     // Parse route function
     if (typeof routeObj == 'function') {
       const route = parseRoute(routePath, routerObj, routerProp, routeObj, cfg)
-      parsedRoutes.push(route)
+      if (route)
+        parsedRoutes.push(route)
     }
   }
 
@@ -134,7 +153,58 @@ function routeFunction(route, routeObj) {
       }
     }
 
-    params.push(callback)
-    routeObj.apply(context, params)
+    // No auth function available, so run route function
+    if (!authRoutes[route.path] || !authRoutes[route.path].fn) {
+      params.push(callback)
+      routeObj.apply(context, params)
+      return
+    }
+
+    // Filter specified but does not contain this route, skip auth function
+    if (authRoutes[route.path].filter && !authRoutes[route.path].filter.includes(routeObj.name)) {
+      params.push(callback)
+      routeObj.apply(context, params)
+      return
+    }
+
+    // Run auth function
+    authRoutes[route.path].fn.apply(context, [(err) => {
+      if (err)
+        return callback(err)
+
+      params.push(callback)
+      routeObj.apply(context, params)
+    }])
   }
+}
+
+function parseAuthRoute(routePath, routeObj, cfg) {
+  if (typeof routeObj == 'function') {
+
+    const route = { path: routePath, fn: routeObj, type: cfg.verbs.auth }
+    return route
+
+  } else if (typeof routeObj == 'object' && !Array.isArray(routeObj)) {
+
+    if (!routeObj.filter || !Array.isArray(routeObj.filter) || !routeObj.fn) {
+      console.log('[WARN]: Improper Auth function structure')
+      return undefined
+    }
+
+    Object.defineProperty(routeObj.fn, 'name', { value: 'auth' })
+    const route = { path: routePath, fn: routeObj.fn, filter: [], type: cfg.verbs.auth }
+
+    for (let filterFn of routeObj.filter) {
+      if (typeof filterFn === 'function') {
+        route.filter.push(filterFn.name)
+      } else if (typeof filterFn === 'string') {
+        route.filter.push(filterFn)
+      }
+    }
+
+    return route
+  }
+
+  console.log('[WARN]: Improper Auth function structure')
+  return undefined
 }
